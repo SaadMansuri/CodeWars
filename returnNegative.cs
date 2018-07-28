38fbcfb1607df538dcd3d48a65e5788acce316da
Matthew Rizzini <matthewrizzini@gmail.com>
2018-07-28 04:05:31 +0000

    added solution for return negative

---
+++ returnNegative.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Kata
+{
+  public static int MakeNegative(int number)
+  {
+    // Code?
+    if (number > 0)
+    {
+      return number * -1;
+    }
+    return number;
+  }
+}
