60b1090e1c5299e22154ce6343e30546664c8ec8
Matthew Rizzini <matthewrizzini@gmail.com>
2018-08-07 16:03:32 +0000

    added solution for coupon code - using TDD practices

--- theCouponCode.cs
+++ theCouponCode.cs
@@ -3,7 +3,17 @@ using System;
 public static class Kata
 {
   public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
-  {            
+  {    
+        // REFACTORED CODE
+        
+        DateTime current = DateTime.Parse(currentDate);
+        DateTime expiration = DateTime.Parse(expirationDate);
+
+        return current <= expiration && enteredCode == correctCode;
+        
+        
+        // ORIGINAL CODE
+        
         // bool isValid;
 
         // if (enteredCode != correctCode)
@@ -11,11 +21,8 @@ public static class Kata
         // else
         //     isValid = true;
             
-        DateTime current = DateTime.Parse(currentDate);
-        DateTime expiration = DateTime.Parse(expirationDate);
-
-        return current <= expiration && enteredCode == correctCode;
-
+        // DateTime current = DateTime.Parse(currentDate);
+        // DateTime expiration = DateTime.Parse(expirationDate);
 
         // if (DateTime.Compare(current, expiration) <= 0)
         //     isValid = true;
