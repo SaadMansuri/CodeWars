ef31c5b11cd882fe12da33535cf8ec4669fab528
Matthew Rizzini <matthewrizzini@gmail.com>
2018-10-25 17:15:44 +0000

    added solution for invert values

---
+++ Java/invertValues.java
@@ -0,0 +1,12 @@
+public class Kata {
+  public static int[] invert(int[] array) {
+  
+  int[] arr = new int[array.length];
+  
+  for (int i = 0; i < array.length; i++) {
+    arr[i] = array[i] * -1;
+  }
+  
+  return arr;
+  }
+}
